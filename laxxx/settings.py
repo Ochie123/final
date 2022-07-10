bc7b2e556ef08986d017cf81f70bb51ba9470f2a
Macbook Air <MacbookAir@ICYDEYNVs-MacBook-Air.local>
2022-07-10 03:57:01 -0400

    hhdg

--- laxxx/settings.py
+++ laxxx/settings.py
@@ -126,8 +126,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
